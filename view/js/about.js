5279e36f9328086a8b207c931d66d4f1721ca8ee
LJX9404 <1353899535@qq.com>
2018-08-21 14:53:29 +0800

    ljx

--- view/js/about.js
+++ view/js/about.js
@@ -1,12 +1,20 @@
-window.onload=function(){
+window.onload = function () {
     new Vue({
         el: '#app',
         data: {
             my: {
                 mytitle: true
+            },
+            active: 0,
+            imgurls: [{ url: "./image/lunbo1.jpg" }, { url: "./image/lunbo2.jpg" }, { url: "./image/lunbo3.jpg" }, { url: "./image/lunbo4.jpg" }, { url: "./image/lunbo5.jpg" }]
+        },
+
+        methods: {
+            next() {
+                if (this.active++ > 2) this.active = 0;
             }
         }
-        
-        })
-        
+
+    })
+
 }
