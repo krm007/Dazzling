885ec34383fb25fd9a75865c0360b872fbf435b2
LJX9404 <1353899535@qq.com>
2018-08-22 09:43:09 +0800

    ljx

--- module/index.js
+++ module/index.js
@@ -4,6 +4,20 @@ module.exports = function () {
     router.get('/', (req, res) => {
         res.render('index');
     });
+
+
+router.get('/about',(req,res)=>{
+    res.render('about')
+});
+
+router.get('/business',(req,res)=>{
+res.render('business')
+});
+
+router.get('/perfect',(req,res)=>{
+res.render('perfect');
+});
+=======
     //关于我们
     router.get('/about', (req, res) => {
         res.render('about')
