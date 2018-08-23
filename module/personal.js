8ae70cd539dececec49fc291fccab5bec2b03915
krm007 <799902293@qq.com>
2018-08-23 13:25:47 +0800

    up

--- module/personal.js
+++ module/personal.js
@@ -3,15 +3,16 @@ module.exports = function () {
     // 显示个人中心页面
     router.get('/', (req, res) => {
         res.render('personal'
-              ,{username:req.session.username,uid:req.session.uid
-              }
+            , {
+                username: req.session.username, uid: req.session.uid
+            }
         );
     });
     //显示注册页面
     router.get('/reg', (req, res) => {
         res.render('reg');
     });
-  
+
     //处理注册的数据
     router.post('/regsubmit', (req, res) => {
         //检查账号是否已经存在
@@ -94,7 +95,7 @@ module.exports = function () {
         res.status(200).send(captcha.data);
     })
 
-    //进到个人中心的前提是登录
+    //进到个人资料修改中心的前提是登录
     router.use((req, res, next) => {
         if (!req.session.uid) {
             res.redirect('/personal/login');
@@ -105,8 +106,37 @@ module.exports = function () {
 
     //进入个人资料修改中心
     router.get('/user', (req, res) => {
-        res.render('user');
+        let sql = 'SELECT * FROM publish WHERE status=0';
+        mydb.query(sql, (err, results) => {
+            if (err) {
+                console.log(err);
+            }
+            // console.log(results);
+            res.render('user', {
+                postlist: results,
+                username: req.session.username
+            });
+        });
     });
+
+    // 删除我的发布
+    router.post('/delmypost',(req,res)=>{
+        let delsql='UPDATE publish SET status=1 WHERE pid=?';
+        mydb.query(delsql,req.body.pid,(err,result)=>{
+            if (err) {
+                console.log(err);
+                res.json({ r: 'db_err' });
+                return;
+            }
+            res.json({ r: 'ok' });
+        });
+    });
+
+
+
+
+
+
     //上传头像
     router.post('/saveheader', (req, res) => {
         let header = req.body.header;
@@ -123,7 +153,7 @@ module.exports = function () {
         let oldpw = req.body.passwd;
         let passwd = req.body.passwd1;
         //验证历史密码是不是正确
-       
+
         let sql = 'SELECT passwd FROM user WHERE uid = ?';
         mydb.query(sql, req.session.uid, (err, data) => {
             if (err) {
@@ -158,7 +188,7 @@ module.exports = function () {
             });
         });
     })
-    
+
 
     //处理退出登录操作
     router.get('/logout', (req, res) => {
@@ -168,20 +198,24 @@ module.exports = function () {
         res.redirect('/personal/login');
     })
 
-    router.get('/publish',(req,res)=>{
+    router.get('/publish', (req, res) => {
         res.render('publish');
-      });
-      router.post('/publish',(req,res)=>{
-        let p=req.body;
-        console.log(req.session.username,req.session.uid);
-        let sql=`INSERT INTO  publish(workname,description,keywords,progress,addtime,imglist,wname,uid,username) VALUES (?,?,?,?,?,?,?,?,?)`
-        mydb.query(sql,[p.workname,p.desc,p.kwd,p.progress,new Date().toLocaleString(),JSON.stringify(p.dialogImageUrl),p.wname,req.session.uid,req.session.username],(err,result)=>{
-            if(err){
-                res.json({r:'db_err'});
-            }else{
-                res.json({r:'success'});
+    });
+    router.post('/publish', (req, res) => {
+        let p = req.body;
+        console.log(req.session.username, req.session.uid);
+        let sql = `INSERT INTO  publish(workname,description,keywords,progress,addtime,imglist,wname,uid,username) VALUES (?,?,?,?,?,?,?,?,?)`
+        mydb.query(sql, [p.workname, p.desc, p.kwd, p.progress, new Date().toLocaleString(), JSON.stringify(p.dialogImageUrl), p.wname, req.session.uid, req.session.username], (err, result) => {
+            if (err) {
+                res.json({ r: 'db_err' });
+            } else {
+                res.json({ r: 'success' });
             }
         })
     });
+
+    //把添加的数据渲染到我的发布中
+    // 将数据库的数据渲染到页面
+
     return router;
 }
